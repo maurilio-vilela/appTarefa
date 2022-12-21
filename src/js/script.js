bc843660884fee2b5b86a8532235b24816c53855
Maurilio Vilela <mauriliovilela20@gmail.com>
2022-12-21 14:00:54 -0300

    Adicionado a função removeTarefa

--- src/js/script.js
+++ src/js/script.js
@@ -3,11 +3,20 @@ const tarefa = document.querySelector("#Tarefa");
 const buttonAction = document.querySelector("#addTarefa");
 
 buttonAction.addEventListener("click", function() {
-    const textTarefa = tarefa.value;
-    tarefa.value = "";
-    listaTarefas.appendChild(addTarefa(textTarefa));
+    if(tarefa.value == ""){
+       /* let error = document.createElement("span");
+        error.setAttribute("class", "error");
+        error.textContent = "Por favor, digite uma tarefa!";
+        listaTarefas.appendChild(error);*/
+        alert("Por favor, digite uma tarefa!");
+    }else{
+        error = "";
+        const textTarefa = tarefa.value;
+        tarefa.value = "";
+        listaTarefas.appendChild(addTarefa(textTarefa));
+    }
+    
 });
-
 function addTarefa(textTarefa) {
     const elementLI = document.createElement("li");
     const elementSpan = document.createElement("span");
@@ -15,6 +24,19 @@ function addTarefa(textTarefa) {
     elementSpan.setAttribute("id", "tarefa");
     elementSpan.textContent = textTarefa;
     elementLI.appendChild(elementSpan);
+    elementLI.appendChild(removeTarefa());
 
     return elementLI;
 }
+function removeTarefa(){
+    const buttonRemove = document.createElement("button");
+    buttonRemove.setAttribute("class", "removeTarefa");
+    buttonRemove.textContent = "X";
+
+    buttonRemove.addEventListener("click", function() {
+        listaTarefas.removeChild(this.parentNode);
+    });
+
+    return buttonRemove;
+}
+
